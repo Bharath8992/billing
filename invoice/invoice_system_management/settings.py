41b1040d43634b8e799eaae677020775491c00ef
BHARATH <bharath122020@gmail.com>
2026-02-12 20:40:28 +0530

    created database

--- invoice/invoice_system_management/settings.py
+++ invoice/invoice_system_management/settings.py
@@ -91,10 +91,15 @@ TEMPLATES = [
 # ======================
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'billing_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 # ======================
 # PASSWORD VALIDATION
